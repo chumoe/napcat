1b79b8ba487aed64baed0e39ffabd303df707742
chumoe <chumoe@foxmail.com>
2026-04-19 00:00:06 +0800

    Add array_to_config helper function

--- src/support/helpers.php
+++ src/support/helpers.php
@@ -16,3 +16,22 @@ if (!function_exists('is_json')) {
         return $array !== null && json_last_error() === JSON_ERROR_NONE;
     }
 }
+
+if (!function_exists('array_to_config')) {
+    /**
+     * 将数组转换为PHP配置文件并保存
+     *
+     * @param array $array 需要转换为配置文件的数组
+     * @param string $path 配置文件的保存路径，默认为'/plugin/chumoe/napcat/config.php'
+     * @return bool 保存成功返回true，失败返回false
+     */
+    function array_to_config(array $array, string $path = '/plugin/chumoe/napcat/config.php'): bool
+    {
+        // 使用var_export将数组转换为字符串形式的PHP代码
+        $export = var_export($array, true);
+        $export = preg_replace('/array \\(/', '[', $export);
+        $export = preg_replace('/\\)$/m', ']', $export);
+        $result = file_put_contents(config_path($path), "<?php\nreturn " . $export . ";\n");
+        return !($result === false);
+    }
+}
